966cdd63f0ab6c9432886366c25df033e37abe0d
ghostplayer170 <82672414+ghostplayer170@users.noreply.github.com>
2023-12-14 11:38:33 +0100

    APP

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import mongoose from "npm:mongoose@7.6.3";
-
 import { load } from "https://deno.land/std@0.204.0/dotenv/mod.ts"; // Leer variables de entorno
 import { typeDefs } from "./typeDefs.ts";
+
 const env = await load(); // Carga Variables de entorno
 
 // Variables de entorno.
-const MONGO_URL = env.MONGO_URL || Deno.env.get("MONGO_URL") || "mongodb+srv://rmontenegrop:Lllubo6BT2sVncJg@clusteruni.pagju8q.mongodb.net/DataBaseRepasoParcial?retryWrites=true&w=majority";
-const PORT = env.PORT || Deno.env.get("PORT") || 4050;
+const MONGO_URL = env.MONGO_URL || Deno.env.get("MONGO_URL");
+const PORT = env.PORT || Deno.env.get("PORT");
 
 // Verifica variables de entorno.
 if (!MONGO_URL || !PORT) {
