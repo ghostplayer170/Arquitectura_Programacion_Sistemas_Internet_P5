228e8915cf510767f9f5689966aaa07aabe347ed
ghostplayer170 <82672414+ghostplayer170@users.noreply.github.com>
2023-12-23 20:44:45 +0100

    App

---
+++ resolvers/entities/indexEntity.ts
@@ -0,0 +1,11 @@
+import { Driver } from "./entityDriver.ts";
+import { Travel } from "./entityTravel.ts";
+import { Client } from "./entityClient.ts";
+
+const Entity = {
+  Driver,
+  Travel,
+  Client,
+};
+
+export default Entity;
--- resolvers/index.ts
+++ resolvers/index.ts
@@ -1,7 +1,9 @@
-import { Query } from "./query/indexQuery.ts"
+import Entity from "./entities/indexEntity.ts";
+import { Query } from "./query/indexQuery.ts";
 import { Mutation } from "./mutation/indexMutation.ts";
 
 export const resolvers = {
+    ...Entity,
     ...Query,
     ...Mutation
 };
