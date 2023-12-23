f8465a791db486e360e51f81eba967f8a50be5e6
ghostplayer170 <82672414+ghostplayer170@users.noreply.github.com>
2023-12-23 19:22:59 +0100

    App

--- db/middlewares/midClient.ts
+++ db/middlewares/midClient.ts
@@ -9,36 +9,38 @@ export const cardPreSave = async function (
   next: () => void,
 ) {
   try {
-    const card: Card = this.cards[this.cards.length - 1];
-    if (validatorsClient.cardNumberFormat(card.number)) {
-      throw new GraphQLError(
-        `Error: Card number ${card.number} is not valid. It must be a number with 16 digits`,
-      );
-    }
-    if (await validatorsClient.cardExists(card.number)) {
-      throw new GraphQLError(
-        `Error: Card number ${card.number} already exists. It must be unique`,
-      );
-    }
-    if (validatorsClient.cardExpirityFormat(card.expirity)) {
-      throw new GraphQLError(
-        `Error: Card expirity ${card.expirity} is not valid. It must be in format MM/YYYY`,
-      );
-    }
-    if (validatorsClient.cardExpirityDate(card.expirity)) {
-      throw new GraphQLError(
-        `Error: Card expirity date ${card.expirity} is not valid. It must be greater or equal than current date`,
-      );
-    }
-    if (validatorsClient.cardCvvFormat(card.cvv)) {
-      throw new GraphQLError(
-        `Error: Card cvv ${card.cvv} is not valid. It must be a number with 3 digits`,
-      );
-    }
-    if (validatorsClient.cardMoneyFormat(card.money)) {
-      throw new GraphQLError(
-        `Error: Card money ${card.money} is not valid. It must be greater or equal than 0`,
-      );
+    if (this.cards && this.cards.length !== 0) {
+      const card: Card = this.cards[this.cards.length - 1];
+      if (validatorsClient.cardNumberFormat(card.number)) {
+        throw new GraphQLError(
+          `Error: Card number ${card.number} is not valid. It must be a number with 16 digits`,
+        );
+      }
+      if (await validatorsClient.cardExists(card.number)) {
+        throw new GraphQLError(
+          `Error: Card number ${card.number} already exists. It must be unique`,
+        );
+      }
+      if (validatorsClient.cardExpirityFormat(card.expirity)) {
+        throw new GraphQLError(
+          `Error: Card expirity ${card.expirity} is not valid. It must be in format MM/YYYY`,
+        );
+      }
+      if (validatorsClient.cardExpirityDate(card.expirity)) {
+        throw new GraphQLError(
+          `Error: Card expirity date ${card.expirity} is not valid. It must be greater or equal than current date`,
+        );
+      }
+      if (validatorsClient.cardCvvFormat(card.cvv)) {
+        throw new GraphQLError(
+          `Error: Card cvv ${card.cvv} is not valid. It must be a number with 3 digits`,
+        );
+      }
+      if (validatorsClient.cardMoneyFormat(card.money)) {
+        throw new GraphQLError(
+          `Error: Card money ${card.money} is not valid. It must be greater or equal than 0`,
+        );
+      }
     }
     next();
   } catch (error) {
